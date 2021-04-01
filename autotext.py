115bb8c28b57f9f1744bf38bda2993f8bd1fef4e
stepthom <sthomas@cs.queensu.ca>
2021-04-01 17:02:29 -0400

    WIP

--- autotext.py
+++ autotext.py
@@ -1,50 +1,26 @@
 import uuid
 import sys
-import logging
-fmt = '%(asctime)s %(levelname)-8s %(message)s'
-logging.basicConfig(level=logging.INFO,
-                    format=fmt,
-                    datefmt='%m-%d %H:%M',
-                    handlers=[
-                        logging.FileHandler("{}.log".format(uuid.uuid4())),
-                        logging.StreamHandler(sys.stdout)
-                        ])
-
+#import logging
 
 import json
 
-import numpy as np
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import NMF
 import textstat
 import itertools
 
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
-
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.model_selection import GroupShuffleSplit, TimeSeriesSplit
 import sklearn
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import NMF
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score, recall_score, precision_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+
 import autosklearn.classification
 from autosklearn.metrics import balanced_accuracy, precision, recall, f1
 
 
-
-
-def print_metrics(pipe, X_test, y_test):
-  y_pred = pipe.predict(X_test)
-
-  print(classification_report(y_test, y_pred))
-
-  df_eval=pd.DataFrame({'Actual':y_test, 'Predicted':y_pred})
-  print(pd.crosstab(df_eval['Actual'],df_eval['Predicted']))
-
-
-
-def get_text_features(df,
-                      text_feature,
+class TextTransformer():
+    def __init__(self,
                       stop_words='english',
                       strip_accents='unicode',
                       lowercase=True,
@@ -54,10 +30,10 @@ def get_text_features(df,
                       ngram_range = [1,1],
                       sublinear_tf = False,
                       get_lexical = True,
-                      n_components = None
-                      ):
+                      n_components = None):
 
-  vectorizer = TfidfVectorizer(
+
+        self.vectorizer = TfidfVectorizer(
                             stop_words=stop_words,
                             strip_accents=strip_accents,
                             lowercase=lowercase,
@@ -67,33 +43,40 @@ def get_text_features(df,
                             ngram_range = ngram_range,
                             sublinear_tf = sublinear_tf)
 
+        if n_components is not None:
+           self.nmf = NMF(n_components=n_components, random_state=1, init='nndsvda', solver='mu', alpha=.1, l1_ratio=.5)
 
-  vectorizer = vectorizer.fit(df[text_feature])
-  dtm = vectorizer.transform(df[text_feature])
-  f = pd.DataFrame(dtm.toarray(),
+        self.n_components = n_components
+        self.get_lexical = get_lexical
+
+    def fit(self, X, y=None):
+        self.vectorizer = self.vectorizer.fit(X)
+        if self.n_components is not None:
+            _dtm = self.vectorizer.transform(X)
+            self.nmf = self.nmf.fit(_dtm)
+        return self
+
+    def transform(self, X, y=None):
+        dtm = self.vectorizer.transform(X)
+        f = pd.DataFrame(dtm.toarray(),
                       columns=['bow_{:s}'.format(name) for name in
-                                vectorizer.get_feature_names()],
-                      index=df.index)
+                                self.vectorizer.get_feature_names()],
+                      index=X.index)
 
-  if n_components is not None:
+        if self.n_components is not None:
+            W = self.nmf.transform(dtm)
+            for i in range(0, self.n_components):
+              f['nmf_{:d}'.format(i)] = W[:,i]
 
-    nmf = NMF(n_components=n_components, random_state=1, init='nndsvda', solver='mu', alpha=.1, l1_ratio=.5)
-    W = nmf.fit_transform(dtm)
-    for i in range(0, n_components):
-      f['nmf_{:d}'.format(i)] = W[:,i]
+        if self.get_lexical:
+          f['lex_len'] = X.apply(lambda x: len(x))
+          f['lex_syllable_count'] = X.apply(lambda x: textstat.syllable_count(x))
+          f['lex_flesch_reading_ease'] = X.apply(lambda x: textstat.flesch_reading_ease(x))
+          f['lex_flesch_kincaid_grade'] = X.apply(lambda x: textstat.flesch_kincaid_grade(x))
+          f['lex_gunning_fog'] = X.apply(lambda x: textstat.gunning_fog(x))
+          #f['lex_coleman_liau_index'] =X.apply(lambda x: textstat.coleman_liau_index(x))
 
-  if get_lexical:
-    f['lex_len'] = df[text_feature].apply(lambda x: len(x))
-    f['lex_syllable_count'] = df[text_feature].apply(lambda x: textstat.syllable_count(x))
-    f['lex_flesch_reading_ease'] = df[text_feature].apply(lambda x: textstat.flesch_reading_ease(x))
-    f['lex_flesch_kincaid_grade'] = df[text_feature].apply(lambda x: textstat.flesch_kincaid_grade(x))
-    f['lex_gunning_fog'] = df[text_feature].apply(lambda x: textstat.gunning_fog(x))
-    #f['lex_coleman_liau_index'] = df[text_feature].apply(lambda x: textstat.coleman_liau_index(x))
-
-  return f
-
-
-
+        return f
 
 
 scorer = autosklearn.metrics.make_scorer(
@@ -101,115 +84,107 @@ scorer = autosklearn.metrics.make_scorer(
         sklearn.metrics.f1_score
     )
 
-def get_auto(X, y):
-
-  feature_names = X.columns
-
-  # regular train/test split
-  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
-
-
-  pipeline_auto = autosklearn.classification.AutoSklearnClassifier(
-        time_left_for_this_task=60, #2*60*60,
-        metric=scorer,
-        n_jobs=10,
-        seed=42,
-        memory_limit=5072,
-        exclude_estimators=['k_nearest_neighbors',  'mlp',
-                    'gaussian_nb', 'bernoulli_nb', 'multinomial_nb',
-                    'decision_tree', 'lda',
-                    'liblinear_svc', 'libsvm_svc', 'qda'],
-        )
-
-  pipeline_auto = pipeline_auto.fit(X_train, y_train, X_test, y_test)
-
-  logging.info("#" * 80)
-  print('metrics')
-  print_metrics(pipeline_auto, X_test, y_test)
-
-  #print("#" * 80)
-  #print('statistics')
-  #print(pipeline_auto.sprint_statistics())
-
-  #print("#" * 80)
-  #print("show_models")
-  #print(pipeline_auto.show_models())
-
-  #def get_metric_result(cv_results):
-  #results = pd.DataFrame.from_dict(cv_results)
-  # results = results[results['status'] == "Success"]
-  # cols = ['rank_test_scores', 'param_classifier:__choice__', 'mean_test_score']
-  # cols.extend([key for key in cv_results.keys() if key.startswith('metric_')])
-  # return results[cols].sort_values(by='rank_test_scores')
-
-  #print("#" * 80)
-  #print("metric results")
-  #print(get_metric_result(pipeline_auto.cv_results_).to_string(index=False))
-
-  return pipeline_auto
-
-
-
-def do_it(df, config): #target_col, text_col):
-    max_features  = config["max_features"]
-    ngram_ranges  = config["ngram_ranges"]
-    sublinear_tfs = config["sublinear_tfs"]
-    stopwords     = config["stopwords"]
-    n_components  = config["n_components"]
-
-    res = []
-
-    for x in itertools.product(max_features, ngram_ranges, sublinear_tfs, stopwords, n_components):
-      logging.info("#" * 80)
-      logging.info("#" * 80)
-      logging.info("Combo: {}".format(x))
-      logging.info("Building features...")
-      features = get_text_features(df, 'en_clean',
-                                   max_features = x[0],
-                                   ngram_range = x[1],
-                                   sublinear_tf = x[2],
-                                   stop_words = x[3],
-                                   n_components = x[4],
-                                   get_lexical=True)
-      logging.info("Done: shape={}".format(features.shape))
-
-      logging.info("Running autosklearn...")
-      #pipe = get_auto(X=features, y=df[target_col])
-      #res.append({'combo': x, 'pipeline': pipe})
-
 
 def main():
+    runname = uuid.uuid4()
+    print("Run name: {}".format(runname))
 
     # Read settings
     with open('settings_small.json') as f:
         config=json.load(f)
 
-    logging.info("Read settings:")
-    logging.info(json.dumps(config, indent=4, sort_keys=True))
+    print("Read settings:")
+    print(json.dumps(config, indent=4, sort_keys=True))
+
+    print("Reading data file...")
+    #df = pd.read_csv("small.csv")
+    df = pd.read_csv(config['filename'])
+    print("Done. Shape = {}".format(df.shape))
 
 
-    logging.info("Reading data file...")
-    #df = pd.read_csv("https://drive.google.com/uc?export=download&id=1dzzVbgHphbCf7kvq9IKiIhwzmxPbuH4s")
-    #df = df.sample(frac=0.02, replace=False, random_state=3)
-    #df.to_csv("small.csv", index=False)
-    df = pd.read_csv("small.csv")
-    logging.info("Done. Shape = {}".format(df.shape))
+    # Kaggle Test Solutions
+    df_kaggle = None
+    if 'test_filename' in config:
+        print("Reading Kaggle test data file...")
+        df_kaggle = pd.read_csv(config['test_filename'])
+        print("Done. Shape = {}".format(df_kaggle.shape))
 
     # Define our Target
-    target_col = 'defaulted'
-    text_feature = 'en_clean'
+    target_col = config['target_col']
+    text_col = config['text_col']
 
-    df[target_col].value_counts()
-
-    # Drop columns that don't make sense: leakage, IDs, etc.
-    drop_list=(['loan_id'])
+    # Drop columns that aren't needed
+    drop_list=[col for col in df.columns if col not in [target_col, text_col]]
     df = df.drop(drop_list, axis=1)
 
     print(df.info())
-    do_it(df, config) #target_col, text_feature)
+    max_features  = config.get("max_features", None)
+    ngram_ranges  = config.get("ngram_ranges", [1, 1])
+    sublinear_tfs = config.get("sublinear_tfs", False)
+    stopwords     = config.get("stopwords", None)
+    n_components  = config.get("n_components", None)
+
+    # regular train/test split
+    X_train, X_test, y_train, y_test = train_test_split(df[text_col], df[target_col], test_size=0.2, random_state=1)
+
+    i = 0
+    for x in itertools.product(max_features, ngram_ranges, sublinear_tfs, stopwords, n_components):
+      print("#" * 80)
+      print("Combo: {}".format(x))
+
+      print("Building features...")
+      tt = TextTransformer(max_features = x[0],
+                           ngram_range = x[1],
+                           sublinear_tf = x[2],
+                           stop_words = x[3],
+                           n_components = x[4],
+                           get_lexical=True)
+      tt = tt.fit(X_train)
+
+      features_train = tt.transform(X_train)
+      print("Done: shape={}".format(features_train.shape))
+
+      print("Running autosklearn...")
+
+
+      pipe = autosklearn.classification.AutoSklearnClassifier(
+            time_left_for_this_task=config['time_left_for_this_task=config'],
+            metric=scorer,
+            n_jobs=1,
+            seed=42,
+            memory_limit=5072,
+            exclude_estimators=config['exclude_estimators'],
+            )
+
+      pipe = pipe.fit(features_train, y_train)
+
+      print("Done fitting autosklearn.")
+
+      def get_metrics(y_true, y_pred):
+          accuracy       = accuracy_score(y_true, y_pred)
+          f1             = f1_score(y_true, y_pred, average="macro")
+          recall         = recall_score(y_true, y_pred, average="macro")
+          precision      = precision_score(y_true, y_pred, average="macro")
+          return [accuracy, f1, recall, precision]
+
+      features_test = tt.transform(X_test)
+      y_pred = pipe.predict(features_test)
+      metrics_test = get_metrics(y_test, y_pred)
+
+      metrics_kaggle = []
+      if df_kaggle is not None:
+        features_kaggle = tt.transform(df_kaggle[text_col])
+        y_pred_kaggle = pipe.predict(features_kaggle)
+        metrics_kaggle = get_metrics(df_kaggle[target_col], y_pred_kaggle)
+
+      lst = [x[0], x[1], x[2], x[3], x[4]] + metrics_test + metrics_kaggle
+
+      print("Results: {}".format(lst))
+      res = pd.DataFrame([lst])
+      res.to_csv("out/{}-{}.csv".format(runname, i), index=False)
+      i = i + 1
 
 
 if __name__ == "__main__":
   main()
-
 
