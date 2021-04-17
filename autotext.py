5d05799fd3b3d7db7e2c3615280e5cf74935c04e
stepthom <sthomas@cs.queensu.ca>
2021-04-17 09:45:51 -0400

    WIP

--- autotext.py
+++ autotext.py
@@ -1,3 +1,6 @@
+from sklearn.linear_model import LogisticRegressionCV
+from flaml import AutoML
+from sklearn.preprocessing import StandardScaler
 import uuid
 import sys
 import argparse
@@ -31,10 +34,12 @@ def get_metrics(y_true, y_pred):
     res['report'] = classification_report(y_true, y_pred, output_dict=True)
     return res
 
+
 scorer = autosklearn.metrics.make_scorer(
     'f1_score',
     sklearn.metrics.f1_score
 )
+
 
 def do_autosklearn(config, features_train, y_train, ffeatures_test, y_test):
     res = {}
@@ -84,8 +89,7 @@ def do_autosklearn(config, features_train, y_train, ffeatures_test, y_test):
 
     return res
 
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.preprocessing import StandardScaler
+
 def do_lr(runname, config, datasets):
     res = {}
     jobs = config.get('n_jobs', 1)
@@ -100,10 +104,10 @@ def do_lr(runname, config, datasets):
     _X_test = scaler.transform(datasets.X_test)
 
     pipe = LogisticRegressionCV(cv=5,
-            Cs=100,
-            max_iter=5000,
-            n_jobs=jobs,
-            random_state=42)
+                                Cs=100,
+                                max_iter=5000,
+                                n_jobs=jobs,
+                                random_state=42)
 
     print("Running LR")
     res['starttime'] = str(datetime.datetime.now())
@@ -129,14 +133,13 @@ def do_lr(runname, config, datasets):
         _preds_df = pd.DataFrame(_preds_test, columns=['y_preds'])
         cols = datasets.X_test.columns.tolist() + ['y_test',  'y_pred']
         _df_test = pd.concat([datasets.X_test.reset_index(drop=True),
-            datasets.y_test.reset_index(drop=True), _preds_df.reset_index(drop=True)], axis=1, ignore_index=True)
+                              datasets.y_test.reset_index(drop=True), _preds_df.reset_index(drop=True)], axis=1, ignore_index=True)
         _df_test.columns = cols
         _df_test.to_csv(_preds_test_fn, index=False)
 
     return datasets, res
 
 
-from flaml import AutoML
 def do_flaml(runname, config, datasets):
     res = {}
     time = config.get('time_left_for_this_task', 100)
@@ -144,20 +147,21 @@ def do_flaml(runname, config, datasets):
 
     pipe = AutoML()
     automl_settings = {
-            "time_budget": time,  # in seconds
-            "metric": 'f1',
-            "task": 'classififcation',
-            "log_file_name": "out/flaml-{}.log".format(runname),
-            "n_jobs": jobs,
-            "estimator_list": ['lgbm', 'xgboost', 'catboost', 'extra_tree'],
-            "model_history": True,
-        }
+        "time_budget": time,  # in seconds
+        "metric": 'f1',
+        "task": 'classififcation',
+        "log_file_name": "out/flaml-{}.log".format(runname),
+        "n_jobs": jobs,
+        "estimator_list": ['lgbm', 'xgboost', 'catboost', 'extra_tree'],
+        "model_history": True,
+    }
 
     res['automl_settings'] = automl_settings
 
     print("Running FLAML with {} jobs for {} seconds...".format(jobs, time))
     res['starttime'] = str(datetime.datetime.now())
-    pipe.fit(datasets.X_train, datasets.y_train, X_val=datasets.X_test, y_val=datasets.y_test, **automl_settings)
+    pipe.fit(datasets.X_train, datasets.y_train, X_val=datasets.X_test,
+             y_val=datasets.y_test, **automl_settings)
     res['endtime'] = str(datetime.datetime.now())
     print("... done running FLAML.")
 
@@ -184,11 +188,12 @@ def do_flaml(runname, config, datasets):
         _preds_df = pd.DataFrame(_preds_test, columns=['y_preds'])
         cols = datasets.X_test.columns.tolist() + ['y_test',  'y_pred']
         _df_test = pd.concat([datasets.X_test.reset_index(drop=True),
-            datasets.y_test.reset_index(drop=True), _preds_df.reset_index(drop=True)], axis=1, ignore_index=True)
+                              datasets.y_test.reset_index(drop=True), _preds_df.reset_index(drop=True)], axis=1, ignore_index=True)
         _df_test.columns = cols
         _df_test.to_csv(_preds_test_fn, index=False)
 
     return datasets, res
+
 
 def dump_results(runname, results):
     with open('out/{}-results.json'.format(runname), 'w') as fp:
@@ -197,13 +202,13 @@ def dump_results(runname, results):
 
 class DataSets:
     def __init__(self, train_fn, val_fn, test_fn,
-           X_train,
-           y_train,
-           X_val,
-           y_val,
-           X_test,
-           y_test
-            ):
+                 X_train,
+                 y_train,
+                 X_val,
+                 y_val,
+                 X_test,
+                 y_test
+                 ):
         self.train_fn = train_fn
         self.val_fn = val_fn
         self.test_fn = test_fn
@@ -214,6 +219,7 @@ class DataSets:
         self.y_val = y_val
         self.X_test = X_test
         self.y_test = y_test
+
 
 def read_and_split(fn, target_col, index_col=None, drop_cols=[]):
     _df = pd.read_csv(fn, index_col=index_col)
@@ -236,21 +242,25 @@ def read_and_split_all(train_fn, val_fn, test_fn, target_col, index_col=None, dr
         raise ValueError('train_fn cannot be null.')
 
     print("Reading train file...")
-    X_train, y_train = read_and_split(train_fn, target_col, index_col, drop_cols)
+    X_train, y_train = read_and_split(
+        train_fn, target_col, index_col, drop_cols)
 
     if test_fn is not None:
         print("Reading test file...")
-        X_test, y_test = read_and_split(test_fn, target_col, index_col, drop_cols)
+        X_test, y_test = read_and_split(
+            test_fn, target_col, index_col, drop_cols)
     else:
         print("Creating test data from training...")
-        X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, test_size=0.1, random_state=1)
+        X_train, X_test, y_train, y_test = train_test_split(
+            X_train, y_train, test_size=0.1, random_state=1)
 
     if val_fn is not None:
         print("Reading val file...")
         X_val, y_val = read_and_split(val_fn, target_col, index_col, drop_cols)
     else:
         print("Creating val data from training data...")
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, random_state=1)
+        X_train, X_val, y_train, y_val = train_test_split(
+            X_train, y_train, test_size=0.1, random_state=1)
 
     if combine_train_and_val:
         print("Combining train and val datasets...")
@@ -260,9 +270,10 @@ def read_and_split_all(train_fn, val_fn, test_fn, target_col, index_col=None, dr
         y_val = None
 
     return DataSets(train_fn, val_fn, test_fn,
-             X_train, y_train,
-             X_val, y_val,
-             X_test, y_test)
+                    X_train, y_train,
+                    X_val, y_val,
+                    X_test, y_test)
+
 
 def main():
 
@@ -286,16 +297,17 @@ def main():
 
     results['settings'] = config
 
-    train_fn   = config.get('train_filename', None)
-    val_fn     = config.get('val_filename', None)
-    test_fn    = config.get('test_filename', None)
-    index_col  = config.get('index_col', None)
+    train_fn = config.get('train_filename', None)
+    val_fn = config.get('val_filename', None)
+    test_fn = config.get('test_filename', None)
+    index_col = config.get('index_col', None)
     target_col = config.get('target_col', None)
-    drop_cols  = config.get('drop_cols', [])
-    combine_train_and_val  = config.get('combine_train_and_val', False)
+    drop_cols = config.get('drop_cols', [])
+    combine_train_and_val = config.get('combine_train_and_val', False)
 
     # Make DataSets
-    datasets = read_and_split_all(train_fn, val_fn, test_fn, target_col, index_col, drop_cols, combine_train_and_val)
+    datasets = read_and_split_all(
+        train_fn, val_fn, test_fn, target_col, index_col, drop_cols, combine_train_and_val)
 
     results['X_train_shape'] = datasets.X_train.shape
     results['y_train_shape'] = datasets.y_train.shape
@@ -318,7 +330,6 @@ def main():
 
     dump_results(runname, results)
     print("Run name: {}".format(runname))
-
 
 
 if __name__ == "__main__":
